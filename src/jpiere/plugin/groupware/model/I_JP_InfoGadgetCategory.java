77681cb9bbc3fd7adf91a1a891f3f53f7ae4e4b9
jpiere <h.hagiwara@compiere-distribution-lab.net>
2020-12-30 19:43:54 +0900

    Move Info Gadget from Dashboard Gadget plugin to Groupware plugin

---
+++ src/jpiere/plugin/groupware/model/I_JP_InfoGadgetCategory.java
@@ -0,0 +1,216 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package jpiere.plugin.groupware.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for JP_InfoGadgetCategory
+ *  @author iDempiere (generated) 
+ *  @version Release 8.2
+ */
+@SuppressWarnings("all")
+public interface I_JP_InfoGadgetCategory 
+{
+
+    /** TableName=JP_InfoGadgetCategory */
+    public static final String Table_Name = "JP_InfoGadgetCategory";
+
+    /** AD_Table_ID=1000036 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 7 - System - Client - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(7);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Client.
+	  * Client/Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within client
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within client
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name Help */
+    public static final String COLUMNNAME_Help = "Help";
+
+	/** Set Comment/Help.
+	  * Comment or Hint
+	  */
+	public void setHelp (String Help);
+
+	/** Get Comment/Help.
+	  * Comment or Hint
+	  */
+	public String getHelp();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name JP_InfoGadgetCategory_ID */
+    public static final String COLUMNNAME_JP_InfoGadgetCategory_ID = "JP_InfoGadgetCategory_ID";
+
+	/** Set Info Gadget Category	  */
+	public void setJP_InfoGadgetCategory_ID (int JP_InfoGadgetCategory_ID);
+
+	/** Get Info Gadget Category	  */
+	public int getJP_InfoGadgetCategory_ID();
+
+    /** Column name JP_InfoGadgetCategory_UU */
+    public static final String COLUMNNAME_JP_InfoGadgetCategory_UU = "JP_InfoGadgetCategory_UU";
+
+	/** Set JP_InfoGadgetCategory_UU	  */
+	public void setJP_InfoGadgetCategory_UU (String JP_InfoGadgetCategory_UU);
+
+	/** Get JP_InfoGadgetCategory_UU	  */
+	public String getJP_InfoGadgetCategory_UU();
+
+    /** Column name JP_PageSize */
+    public static final String COLUMNNAME_JP_PageSize = "JP_PageSize";
+
+	/** Set Page Size	  */
+	public void setJP_PageSize (int JP_PageSize);
+
+	/** Get Page Size	  */
+	public int getJP_PageSize();
+
+    /** Column name JP_UserDisplayPosition */
+    public static final String COLUMNNAME_JP_UserDisplayPosition = "JP_UserDisplayPosition";
+
+	/** Set User Display Position.
+	  * JPIERE-0110:JPPS
+	  */
+	public void setJP_UserDisplayPosition (String JP_UserDisplayPosition);
+
+	/** Get User Display Position.
+	  * JPIERE-0110:JPPS
+	  */
+	public String getJP_UserDisplayPosition();
+
+    /** Column name MaxQueryRecords */
+    public static final String COLUMNNAME_MaxQueryRecords = "MaxQueryRecords";
+
+	/** Set Max Query Records.
+	  * If defined, you cannot query more records as defined - the query criteria needs to be changed to query less records
+	  */
+	public void setMaxQueryRecords (int MaxQueryRecords);
+
+	/** Get Max Query Records.
+	  * If defined, you cannot query more records as defined - the query criteria needs to be changed to query less records
+	  */
+	public int getMaxQueryRecords();
+
+    /** Column name Name */
+    public static final String COLUMNNAME_Name = "Name";
+
+	/** Set Name.
+	  * Alphanumeric identifier of the entity
+	  */
+	public void setName (String Name);
+
+	/** Get Name.
+	  * Alphanumeric identifier of the entity
+	  */
+	public String getName();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+
+    /** Column name Value */
+    public static final String COLUMNNAME_Value = "Value";
+
+	/** Set Search Key.
+	  * Search key for the record in the format required - must be unique
+	  */
+	public void setValue (String Value);
+
+	/** Get Search Key.
+	  * Search key for the record in the format required - must be unique
+	  */
+	public String getValue();
+}
---
+++ src/jpiere/plugin/groupware/model/MInfoGadgetCategory.java
@@ -0,0 +1,137 @@
+/******************************************************************************
+ * Product: JPiere                                                            *
+ * Copyright (C) Hideaki Hagiwara (h.hagiwara@oss-erp.co.jp)                  *
+ *                                                                            *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY.                          *
+ * See the GNU General Public License for more details.                       *
+ *                                                                            *
+ * JPiere is maintained by OSS ERP Solutions Co., Ltd.                        *
+ * (http://www.oss-erp.co.jp)                                                 *
+ *****************************************************************************/
+
+package jpiere.plugin.groupware.model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import org.compiere.model.Query;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
+import org.compiere.util.Util;
+
+public class MInfoGadgetCategory extends X_JP_InfoGadgetCategory {
+
+	public MInfoGadgetCategory(Properties ctx, int JP_InfoGadgetCategory_ID,
+			String trxName) {
+		super(ctx, JP_InfoGadgetCategory_ID, trxName);
+	}
+
+	public MInfoGadgetCategory(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+
+	public MInfoGadget[] getInfoGadgets(String whereClause, String orderClause, int maxRows)
+	{
+
+		StringBuilder whereClauseFinal = new StringBuilder(MInfoGadget.COLUMNNAME_JP_InfoGadgetCategory_ID +"=? ");
+		if (!Util.isEmpty(whereClause, true))
+			whereClauseFinal.append(whereClause);
+		if (orderClause.length() == 0)
+			orderClause = MInfoGadget.COLUMNNAME_Date1;
+		//
+//		List<MInfoGadget> list = new Query(Env.getCtx(), MInfoGadget.Table_Name, whereClauseFinal.toString(), get_TrxName())
+//										.setParameters(get_ID())
+//										.setOrderBy(orderClause)
+//										.list();
+
+		ArrayList<MInfoGadget> list = new ArrayList<MInfoGadget>();
+		StringBuilder sql = new StringBuilder("SELECT * FROM " + MInfoGadget.Table_Name + " WHERE " + MInfoGadget.COLUMNNAME_JP_InfoGadgetCategory_ID + "=" + get_ID());
+							sql.append(whereClause);
+							sql.append(" ORDER BY ").append(orderClause);
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			pstmt = DB.prepareStatement(sql.toString(), get_TrxName());
+			if (maxRows > 0)
+			{
+				pstmt.setMaxRows(maxRows);
+			}
+			rs = pstmt.executeQuery();
+			while (rs.next())
+				list.add(new MInfoGadget (getCtx(), rs, get_TrxName()));
+		}
+		catch (Exception e)
+		{
+			log.log(Level.SEVERE, sql.toString(), e);
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null;
+			pstmt = null;
+		}
+
+		return list.toArray(new MInfoGadget[list.size()]);
+	}
+
+	/**	Info Gadget belon to Category					*/
+	protected MInfoGadget[] 	m_InfoGadgets = null;
+
+	/**
+	 * 	Get Lines of Order
+	 * 	@param requery requery
+	 * 	@param orderBy optional order by column
+	 * 	@return lines
+	 */
+	public MInfoGadget[] getInfoGadgets (boolean requery, String orderBy)
+	{
+		if (m_InfoGadgets != null && !requery) {
+			set_TrxName(m_InfoGadgets, get_TrxName());
+			return m_InfoGadgets;
+		}
+		//
+		String orderClause = "";
+		if (orderBy != null && orderBy.length() > 0)
+			orderClause += orderBy;
+		else
+			orderClause += "Date1 DESC";
+
+		m_InfoGadgets = getInfoGadgets(null, orderClause, 0);
+		return m_InfoGadgets;
+	}	//	getLines
+
+	/**
+	 * 	Get Lines of Order.
+	 * 	(used by web store)
+	 * 	@return lines
+	 */
+	public MInfoGadget[] getInfoGadgets()
+	{
+		return getInfoGadgets(false, null);
+	}	//	getLines
+
+	/**
+	 * 	Get BPartner with Value
+	 *	@param ctx context
+	 *	@param Value value
+	 *	@return BPartner or null
+	 */
+	public static MInfoGadgetCategory get (Properties ctx, String Value)
+	{
+		if (Value == null || Value.length() == 0)
+			return null;
+		final String whereClause = "Value=? AND AD_Client_ID=?";
+		MInfoGadgetCategory retValue = new Query(ctx, MInfoGadgetCategory.Table_Name, whereClause, null)
+		.setParameters(Value,Env.getAD_Client_ID(ctx))
+		.firstOnly();
+		return retValue;
+	}	//	get
+
+}
